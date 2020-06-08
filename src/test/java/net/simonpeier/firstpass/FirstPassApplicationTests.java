0b5114337bb2a7f834c5f4ac0b404a37b18db306
Simon Peier <simon.peier@comerge.net>
2020-06-08 12:05:48 +0200

    Initial Commit

---
+++ src/test/java/net/simonpeier/firstpass/FirstPassApplicationTests.java
@@ -0,0 +1,13 @@
+package net.simonpeier.firstpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
