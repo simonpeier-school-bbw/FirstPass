c2fd3ebf7f3fd75b0f73952f9c1f30f3061d963a
Simon Peier <simon.peier@comerge.net>
2020-06-12 19:04:33 +0200

    Add login page and related controller

--- src/main/java/net/simonpeier/firstpass/LoginController.java
+++ src/main/java/net/simonpeier/firstpass/LoginController.java
@@ -1,4 +1,13 @@
 package net.simonpeier.firstpass;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+
+    @GetMapping("/login")
+    public String showLogin() {
+        return "login";
+    }
 }
