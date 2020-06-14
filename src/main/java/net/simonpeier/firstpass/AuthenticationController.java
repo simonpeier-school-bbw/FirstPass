a12edbab0f2805c00e330f53ac24db7b95b878ba
philipp012 <philipp.kiss@bluewin.ch>
2020-06-14 17:09:45 +0200

    Add POJO for User and Application

--- src/main/java/net/simonpeier/firstpass/AuthenticationController.java
+++ src/main/java/net/simonpeier/firstpass/AuthenticationController.java
@@ -4,10 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
+public class AuthenticationController {
 
     @GetMapping("/login")
     public String showLogin() {
         return "login";
     }
+
+
+
 }
---
+++ src/main/java/net/simonpeier/firstpass/User/User.java
@@ -0,0 +1,42 @@
+package net.simonpeier.firstpass.User;
+
+import java.util.List;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private List<Application> applications;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Application> getApplications() {
+        return applications;
+    }
+
+    public void setApplications(List<Application> applications) {
+        this.applications = applications;
+    }
+}
