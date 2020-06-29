31fd5d3126335bbd7189f2bdd2ffaea558c7fe66
philipp012 <philipp.kiss@bluewin.ch>
2020-06-29 15:49:24 +0200

    Change hashing-iteration count to 50k

--- src/main/java/net/simonpeier/firstpass/security/Cypher.java
+++ src/main/java/net/simonpeier/firstpass/security/Cypher.java
@@ -1,13 +1,25 @@
 package net.simonpeier.firstpass.security;
 
-import javax.crypto.SecretKeyFactory;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.simonpeier.firstpass.model.Application;
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import javax.crypto.*;
 import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
+import java.util.List;
 
 public class Cypher {
+
     public String generateSalt() {
         SecureRandom random = new SecureRandom();
         byte[] salt = new byte[16];
@@ -19,7 +31,7 @@ public class Cypher {
 
     // hashes password of given User using the provided salt
     public String hashPassword(String password, String salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 128);
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 50000, 128);
         SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
 
         byte[] hash = factory.generateSecret(spec).getEncoded();
